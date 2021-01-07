2ceee9ed3473e81ec32db79fa32c43348f8731cb
cbragg3136 <cbragg3136@gmail.com>
2021-01-06 19:21:08 -0500

    render_template app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_pymongo import PyMongo
 from os import environ
 
@@ -11,7 +11,7 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/api/appid-mongo')
 def getAppidMongo():
